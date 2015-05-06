ad39c1108b9fce0613b954c3691361e16b4c4bf2
Lorena Morales <lorenamorales4455@gmail.com>
2015-05-06 12:25:04 -0500

    popups

--- public_html/js/share.js
+++ public_html/js/share.js
@@ -9,7 +9,7 @@
  * 
  */
 $(document).ready(function() {
-   if (location.search.substr(1)) {
+    if (location.search.substr(1)) {
         Variable = location.search.substr(1);
         var elem = Variable.split('&');
         id_tema = elem[0];
@@ -19,18 +19,18 @@ $(document).ready(function() {
     var url;
     url = "grafica.html";
     /*switch (id_tema) {
-        case "1":
-            url = "grafica.html";
-            break;
-        case "2":
-            url = "relvGrafica.html";
-            break;
-        case "4":
-            url = "grafica.html";
-            break;
-        default:
-            url = "";
-    }*/
+     case "1":
+     url = "grafica.html";
+     break;
+     case "2":
+     url = "relvGrafica.html";
+     break;
+     case "4":
+     url = "grafica.html";
+     break;
+     default:
+     url = "";
+     }*/
 
     var pageTitle = document.title + " - SISEcuador "; //HTML page title
     var pageUrl = 'http://192.168.50.76:8383/share/' + url + '?' + id_tema + '&' + id_ib + '&' + id_subtema; //Location of the page
@@ -40,36 +40,39 @@ $(document).ready(function() {
     //user clicks on a share button
     $('.button-wrap').click(function(event) {
         var shareName = $(this).attr('class').split(' ')[0]; //get the first class name of clicked element
+        /*
+         switch (shareName) //switch to different links based on different social name
+         {
+         case 'facebook':
+         //openLink = 'http://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(pageUrl) + '&amp;title=' + encodeURIComponent(pageTitle);
+         openLink = 'http://m.facebook.com/sharer.php?u=' + encodeURIComponent(pageUrl) + '&t=' + encodeURIComponent('Miniterio Coordinador de Desarrollo Social');
+         //alert('facebook:'+openLink);
+         break;
+         case 'twitter':
+         //openLink = 'http://twitter.com/home?status=' + encodeURIComponent(pageTitle + ' ' + pageUrl);                
+         openLink = 'http://www.twitter.com/intent/tweet?text=' + encodeURIComponent('Compartiendo información desde #SISEcuador Sistema de Información Social del Ecuador del @SocialEc #MCDS') + '&url=' + encodeURIComponent(pageUrl);
+         //alert('twitter:'+openLink);
+         break;
+         case 'google':
+         openLink = 'https://plus.google.com/share?url=' + encodeURIComponent(pageUrl) + '&amp;title=' + encodeURIComponent(pageTitle);
+         //alert('google:'+openLink);
+         break;
+         case 'email':
+         openLink = 'mailto:?subject=' + pageTitle
+         + '&body= Se ha compartido información del Miniterio Coordinador de Desarrollo Social - MCDS ' + encodeURIComponent(pageUrl);
+         //alert('email:'+openLink);
+         break;
+         case 'whatsapp':
+         //$('#btnWhatsApp').attr("href","whatsapp://send?text=Compartiendo informacion desde "+encodeURIComponent(pageUrl));
+         openLink = 'whatsapp://send?text= Información compartida desde la aplicación "Sistema de Informacion Social del Ecuador SISEcuador" del Miniterio Coordinador de Desarrollo Social MCDS. ' + encodeURIComponent(pageUrl);
+         //alert('whatsapp:'+openLink);
+         break;
+         
+         }*/
 
-        switch (shareName) //switch to different links based on different social name
-        {
-            case 'facebook':
-                //openLink = 'http://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(pageUrl) + '&amp;title=' + encodeURIComponent(pageTitle);
-                openLink = 'http://m.facebook.com/sharer.php?u=' + encodeURIComponent(pageUrl) + '&t=' + encodeURIComponent('Miniterio Coordinador de Desarrollo Social');
-                //alert('facebook:'+openLink);
-                break;
-            case 'twitter':
-                //openLink = 'http://twitter.com/home?status=' + encodeURIComponent(pageTitle + ' ' + pageUrl);                
-                openLink = 'http://www.twitter.com/intent/tweet?text=' + encodeURIComponent('Compartiendo información desde #SISEcuador Sistema de Información Social del Ecuador del @SocialEc #MCDS') + '&url=' + encodeURIComponent(pageUrl);
-                //alert('twitter:'+openLink);
-                break;
-            case 'google':
-                openLink = 'https://plus.google.com/share?url=' + encodeURIComponent(pageUrl) + '&amp;title=' + encodeURIComponent(pageTitle);
-                //alert('google:'+openLink);
-                break;
-            case 'email':
-                openLink = 'mailto:?subject=' + pageTitle
-                        + '&body= Se ha compartido información del Miniterio Coordinador de Desarrollo Social - MCDS ' + encodeURIComponent(pageUrl);
-                //alert('email:'+openLink);
-                break;
-            case 'whatsapp':
-                //$('#btnWhatsApp').attr("href","whatsapp://send?text=Compartiendo informacion desde "+encodeURIComponent(pageUrl));
-                openLink = 'whatsapp://send?text= Información compartida desde la aplicación "Sistema de Informacion Social del Ecuador SISEcuador" del Miniterio Coordinador de Desarrollo Social MCDS. ' + encodeURIComponent(pageUrl);
-                //alert('whatsapp:'+openLink);
-                break;
-
+        if (shareName === 'twitter') {
+            window.open('http://www.twitter.com/intent/tweet?text=' + encodeURIComponent('Compartiendo información desde #SISEcuador Sistema de Información Social del Ecuador del @SocialEc #MCDS') + '&url=' + encodeURIComponent(pageUrl), 'popup', 'width=600,height=450');
         }
-
         //Parameters for the Popup window
         winWidth = 650;
         winHeight = 450;
@@ -81,8 +84,8 @@ $(document).ready(function() {
         //alert(openLink); 
         //javascript:window.open(openLink, 'Compartir este enlace', winOptions);
         //alert(winOptions);
-        window.open(openLink, 'popup', 'width=600,height=450');
-        
+        //window.open(openLink, 'popup', 'width=600,height=450');
+
         return false;
     });
 
